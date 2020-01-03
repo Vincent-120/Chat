74a3a4744b2a0a435b37a10e199d19df2edd392f
Boumarouane <i.boumarouane@gmail>
2020-01-03 16:02:19 +0100

    ajout de la deuxième parti du tuto

--- public/client.js
+++ public/client.js
@@ -1,21 +1,74 @@
 var socket = io();
 
-$('form').submit(function(e) {
-	e.preventDefault(); // On évite le recharchement de la page lors de la validation du formulaire
-    // On crée notre objet JSON correspondant à notre message
-	var message = {
-		text : $('#m').val()
-	}
-	socket.emit('chat-message', message); // On émet l'événement avec le message associé
-    $('#m').val(''); // On vide le champ texte
-    if (message.text.trim().length !== 0) { // Gestion message vide
-    socket.emit('chat-message', message);
-    }
+/**
+ * Connexion d'un utilisateur
+ */
+$('#login form').submit(function (e) {
+  e.preventDefault();
+  var user = {
+    username : $('#login input').val().trim()
+  };
+  if (user.username.length > 0) { // Si le champ de connexion n'est pas vide
+    socket.emit('user-login', user);
+    $('body').removeAttr('id'); // Cache formulaire de connexion
     $('#chat input').focus(); // Focus sur le champ du message
+  }
 });
-  /**
-   * Réception d'un message
-   */
-  socket.on('chat-message', function (message) {
-    $('#messages').append($('<li>').text(message.text));
+
+/**
+ * Envoi d'un message
+ */
+$('#chat form').submit(function (e) {
+  e.preventDefault();
+  var message = {
+    text : $('#m').val()
+  };
+  $('#m').val('');
+  if (message.text.trim().length !== 0) { // Gestion message vide
+    socket.emit('chat-message', message);
+  }
+  $('#chat input').focus(); // Focus sur le champ du message
+});
+
+/**
+ * Réception d'un message
+ */
+socket.on('chat-message', function (message) {
+  $('#messages').append($('<li>').html('<span class="username">' + message.username + '</span> ' + message.text));
+  scrollToBottom();
+});
+
+/**
+ * Réception d'un message de service
+ */
+socket.on('service-message', function (message) {
+  $('#messages').append($('<li class="' + message.type + '">').html('<span class="info">information</span> ' + message.text));
+  scrollToBottom();
+});
+
+/**
+ * Scroll vers le bas de page si l'utilisateur n'est pas remonté pour lire d'anciens messages
+ */
+function scrollToBottom() {
+  if ($(window).scrollTop() + $(window).height() + 2 * $('#messages li').last().outerHeight() >= $(document).height()) {
+    $("html, body").animate({ scrollTop: $(document).height() }, 0);
+  }
+}
+
+/**
+ * Connexion d'un nouvel utilisateur
+ */
+socket.on('user-login', function (user) {
+  $('#users').append($('<li class="' + user.username + ' new">').html(user.username));
+  setTimeout(function () {
+    $('#users li.new').removeClass('new');
+  }, 1000);
+});
+
+/**
+ * Déconnexion d'un utilisateur
+ */
+socket.on('user-logout', function (user) {
+  var selector = '#users li.' + user.username;
+  $(selector).remove();
 });
--- server.js
+++ server.js
@@ -1,11 +1,65 @@
+/**
+ * List of connected users
+ */
+var users = [];
+
 // Tout d'abbord on initialise notre application avec le framework Express 
 // et la bibliothèque http integrée à node.
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+
 // On gère les requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
 app.use("/", express.static(__dirname + "/public"));
+
+io.on('connection', function(socket){
+    /**
+       * Utilisateur connecté à la socket
+     */
+    var loggedUser;
+
+    console.log('a user connected');
+
+    /**
+   * Déconnexion d'un utilisateur : broadcast d'un 'service-message'
+   */
+  socket.on('disconnect', function () {
+    if (loggedUser !== undefined) {
+      console.log('user disconnected : ' + loggedUser.username);
+      var serviceMessage = {
+        text: 'User "' + loggedUser.username + '" disconnected',
+        type: 'logout'
+      };
+      socket.broadcast.emit('service-message', serviceMessage);
+    }
+  });
+
+    /**
+       * Connexion d'un utilisateur via le formulaire
+     */
+    socket.on('user-login', function (loggedUser) {
+      console.log('user logged in : ' + loggedUser.username);
+      user = loggedUser;
+      if (loggedUser !== undefined) {
+        var serviceMessage = {
+          text: 'User "' + loggedUser.username + '" logged in',
+          type: 'login'
+        };
+        socket.broadcast.emit('service-message', serviceMessage);
+      }
+      /**
+   * Réception de l'événement 'chat-message' et réémission vers tous les utilisateurs
+   */
+    socket.on('chat-message', function (message) {
+    message.username = loggedUser.username; // On intègre ici le nom d'utilisateur au message
+    io.emit('chat-message', message);
+    console.log('Message de : ' + loggedUser.username);
+  });
+    });
+
+  });
 
 // On lance le serveur en écoutant les connexions arrivant sur le port 3000
 http.listen(3000, function(){
@@ -13,16 +67,4 @@ http.listen(3000, function(){
   
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
-    });
-    /**
-   * Réception de l'événement 'chat-message' et réémission vers tous les utilisateurs
-   */
-  socket.on('chat-message', function (message) {
-    io.emit('chat-message', message);
-    });
-  });
 
