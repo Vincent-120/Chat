6a24e4520dd249ae22cb63b63bf921a055d712e0
Vincent <v1996.vincent@gmail.com>
2020-01-08 12:09:42 +0100

    clean branch

--- index.js
+++
@@ -1,8 +0,0 @@
-// let http = require('http');
-// let port = 8081;
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write('Salut tout le monde !');
-//     res.end();
-// }).listen(port);
-// console.log("Serveur tourne sur http://localhost:" + port);
